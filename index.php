07b2eac976973905726073b48933053d6c7e9a45
Mustafa Shaaban <mustafashaaban22@gmail.com>
2018-04-20 02:25:09 +0200

    first version of dashboard

---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Title</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" >
+        <link rel="stylesheet" href="css/fontawesome-all.css" >
+        <link rel="stylesheet" href="css/main.css" >
+    </head>
+    <body>
+
+        <div class="main-body">
+
+            <div class="left-body">
+                <nav class="left-nav">
+                    <div class="logo">
+                        <h4>Admin Panel</h4>
+                    </div>
+                </nav>
+                <aside class="aside">
+                    <div class="profile">
+                        <img src="images/personal.png" alt="Profile Picture" />
+                        <h4>Mustafa Shaaban</h4>
+                        <p>Admin</p>
+                    </div>
+                    <div class="menu">
+                        <a href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                        <a href="#"><i class="fas fa-users"></i> Employee</a>
+                        <a href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                        <a href="#"><i class="fas fa-users"></i> Employee</a>
+                        <a href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                        <a href="#"><i class="fas fa-users"></i> Employee</a>
+                        <a href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                        <a href="#"><i class="fas fa-users"></i> Employee</a>
+                        <a href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                        <a href="#"><i class="fas fa-users"></i> Employee</a>
+                        <a href="#"><i class="fas fa-sign-out-alt"></i> Sign out</a>
+                    </div>
+                </aside>
+
+            </div>
+
+            <div class="right-body">
+                <nav class="right-nav">
+                    <div class="left">
+                        <button class="btn btn-default nav-btn">
+                            <i class="fas fa-bars fa-1x"></i>
+                        </button>
+                    </div>
+                    <div class="right">
+                        <div class="dropdown notification">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="far fa-bell"></i>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+                        <div class="dropdown messages">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="far fa-envelope"></i>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+                        <div class="dropdown profile">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img src="images/personal.png" alt="Profile Picture" /> Mustafa Shaaban
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Profile</a>
+                                <a class="dropdown-item" href="#">Change Password</a>
+                                <a class="dropdown-item" href="#">Account Setting</a>
+                                <a class="dropdown-item" href="#">Sign out</a>
+                            </div>
+                        </div>
+                        <div class="dropdown language">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-globe"></i>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Arabic</a>
+                                <a class="dropdown-item" href="#">English</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+                <div class="body">
+                    <h1>Home Page</h1>
+                </div>
+                <footer class="footer">
+                    <p>All CopyRights are Reserved for Mustafa Shaaban</p>
+                </footer>
+
+            </div>
+
+
+        </div>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $(".nav-btn").click(function(){
+        $('.left-body').toggleClass('close-nav');
+    });
+});
